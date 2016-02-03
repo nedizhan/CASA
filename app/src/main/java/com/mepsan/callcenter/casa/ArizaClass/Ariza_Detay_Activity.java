68b0fdbac7f113cad4f2e501f44ae800251342cb
DELL <oguzhan_uyanik@hotmail.com>
2016-02-03 10:18:43 +0200

    10:11 03.02.2016

--- app/src/main/java/com/mepsan/callcenter/casa/ArizaClass/Ariza_Detay_Activity.java
+++ app/src/main/java/com/mepsan/callcenter/casa/ArizaClass/Ariza_Detay_Activity.java
@@ -81,6 +81,7 @@ public class Ariza_Detay_Activity extends AppCompatActivity {
 
     }
 
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
